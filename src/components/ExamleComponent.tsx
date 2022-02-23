2a10dc612d6d1df377c4afd45c94bbbddebe43c1
Franklin Shera <fshera96@gmail.com>
2022-02-23 17:38:08 +0300

    Example Component

---
+++ src/components/ExamleComponent.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const ExamleComponent = () => {
+  return (
+    <View>
+      <Text>ExamleComponent</Text>
+    </View>
+  );
+};
+
+export default ExamleComponent;
+
+const styles = StyleSheet.create({});
